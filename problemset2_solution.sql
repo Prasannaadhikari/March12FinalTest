62267076f0eb787db4f96fbe8580eae4e44988a6
Prasanna Adhikari <36228386+Prasannaadhikari@users.noreply.github.com>
2022-03-12 16:41:25 +0545

    Add files via upload

---
+++ problemset2_solution.sql
@@ -0,0 +1,7 @@
+SELECT d.Name AS Department, e1.Name AS Employee, e1.Salary 
+FROM Employee e1
+INNER JOIN Department d ON e1.DepartmentId = d.Id
+WHERE 3 > (SELECT COUNT(DISTINCT e2.Salary)
+			FROM Employee e2
+			WHERE e2.Salary > e1.Salary AND e1.DepartmentId = e2.DepartmentId)
+ORDER BY Department, Salary DESC;
